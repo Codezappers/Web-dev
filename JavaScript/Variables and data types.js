652ab717ac97fd6843ed98d173f1e626defa825e
Aden John <Adenthedev@gmail.com>
2024-03-21 11:19:41 -0600

    objects in javaScript

--- JavaScript/Variables and data types.js
+++ JavaScript/Variables and data types.js
@@ -2,6 +2,12 @@ console.log("Hello world!");
 
 PreviousAge = 24;
 MyCurrentAge = PreviousAge + 1
-console.log(MyCurrentAge)
+console.log(MyCurrentAge);
 
-
+const product = {
+    name: "ball pen",
+    rating: 4.5,
+    offer: 5,
+    price: 270,
+}
+console.log( typeof product["name"]);
